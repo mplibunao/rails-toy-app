5aabefc435321eeca3351e9ce7b9d73faca1a814
mplibunao <markpaololibunao@gmail.com>
2018-03-17 21:16:31 +0800

    Generates Micropost scaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
+ActiveRecord::Schema.define(version: 20180317131040) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+=======
 ActiveRecord::Schema.define(version: 20180317073428) do
+>>>>>>> c7bbd26... Generates user model scaffolding
 
   create_table "users", force: :cascade do |t|
     t.string "name"
