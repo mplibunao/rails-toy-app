ee69b5735702480f1a5d36c52fcf780831f0e911
mplibunao <markpaololibunao@gmail.com>
2018-03-17 21:19:17 +0800

    Fixes merge conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   resources :microposts
-=======
->>>>>>> c7bbd26... Generates user model scaffolding
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#index'
